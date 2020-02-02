05ec2801d97e81a99db5a79ca39d8f78ed698fc5
Florian Schwanz <florian.schwanz@interoberlin.de>
2020-02-02 13:30:59 +0100

    Add German taggers

---
+++ src/main/kotlin/PosTagger.kt
@@ -0,0 +1,11 @@
+package berlin.florianschwanz
+
+object PosTagger {
+
+    private val basePath = "edu/stanford/nlp/models/pos-tagger/"
+
+    val ENGLISH_LEFT3_WORDS_DISTSIM = basePath + "english-left3words/english-left3words-distsim.tagger"
+    val GERMAN_FAST_TAGGER = basePath + "german/german-fast.tagger"
+    val GERMAN_HGC_TAGGER = basePath + "german/german-hgc.tagger"
+    val GERMAN_UD_TAGGER = basePath + "german/german-ud.tagger"
+}
